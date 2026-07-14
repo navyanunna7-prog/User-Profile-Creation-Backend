23032499c985337be374e7b18befda875857c834
navyanunna7-prog <navyanunna7@gmail.com>
2026-07-14 13:43:30 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
+const dns=require("dns")
+dns.setServers(["8.8.8.8","8.8.4.4"])
 
 const connectDB = async () => {
   try {
     if (!process.env.MONGO_URI) {
       throw new Error("MONGO_URI is not defined. Add it to your .env file.");
     }
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb+srv://navyanunna:2507@cluster0.ozhpq3c.mongodb.net/userprofile");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
